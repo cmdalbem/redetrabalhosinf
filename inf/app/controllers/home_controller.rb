2907c855adf4791b2d34d321c5e782d1d38d9462
Cristiano Medeiros Dalbem <ninja.dalbem@gmail.com>
2012-10-16 13:13:14 -0300

    initial version

---
+++ inf/app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def show
+  	render :action => 'show'
+  end
+end
---
+++ inf/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inf::Application.config.secret_token = 'c2b1b3f432ee22d1b9115b0fcfb6f8bea4b8ccfd3e782b5e558f11b32e1e8c52f53fb88697470822e9d43510b3201f8ddb463571aecc566291752db64206aa50'
