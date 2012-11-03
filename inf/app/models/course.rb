92bbd2e76f481457daa1a9a3c0b026be3b1b7675
Cristiano Medeiros Dalbem <ninja.dalbem@gmail.com>
2012-11-03 05:44:03 -0200

    Adds rake:seed script for poppulating Courses DB
    With 'db\seeds.rb' one may automatically poppulate his local Courses
    database table with all disciplines INF disciplines.

--- inf/app/models/course.rb
+++ inf/app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
 	has_many :projects
 
-	validates :name, :presence => true
-	validates :code, :presence => true
+	validates :name, presence: true
+	validates :code, presence: true, uniqueness: true
 
 end
