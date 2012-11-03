b5e06c3fa56caccf504f2942121da08fe517a718
Cristiano Medeiros Dalbem <ninja.dalbem@gmail.com>
2012-11-03 06:57:08 -0200

    Small fix.

--- inf/app/helpers/application_helper.rb
+++ inf/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def sortable(title, column)
 		direction = params[:direction] == "asc" ? "desc" : "asc"
   		css_class = column == params[:sort] ? "current #{params[:direction]}" : nil
-  		
-		link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+
+		link_to title, {:sort => column, :direction => direction, :search => params[:search]}, {:class => css_class}
 	end
 end
