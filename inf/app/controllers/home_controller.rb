3db8a7c83d5e4910fd2b8d346feeccf7e58f67c6
Lucas Zawacki <lfzawacki@gmail.com>
2013-01-07 21:14:36 -0200

    [views] Added project count on Home.
    When the design breaks we'll be happy because we'll have a crapload of projects registered

--- inf/app/controllers/home_controller.rb
+++ inf/app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
   def show
-  	@lastProjects = Project.order("created_at DESC").limit(5).all
-  	@lastPeople = Person.order("created_at DESC").limit(5).all
+    @lastProjects = Project.order("created_at DESC").limit(5).all
+    @lastPeople = Person.order("created_at DESC").limit(5).all
+    @project_count = Project.count
 
-  	render :action => 'show'
+    render :action => 'show'
   end
 
   def about
-  	render :action => 'about'
+    render :action => 'about'
   end
 end
