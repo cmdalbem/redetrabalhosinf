d4245777702946af8523b37e91b280551cfe1866
Cristiano Medeiros Dalbem <ninja.dalbem@gmail.com>
2013-03-28 12:01:54 -0300

    Adds "cog button" on projects' table.

--- inf/db/schema.rb
+++ inf/db/schema.rb
@@ -41,36 +41,12 @@ ActiveRecord::Schema.define(:version => 20130323181331) do
   add_index "comments", ["person_id"], :name => "index_comments_on_person_id"
   add_index "comments", ["project_id"], :name => "index_comments_on_project_id"
 
-  create_table "conversations", :force => true do |t|
-    t.string   "subject",    :default => ""
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-  end
-
   create_table "courses", :force => true do |t|
     t.string   "name"
     t.string   "code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  create_table "notifications", :force => true do |t|
-    t.string   "type"
-    t.text     "body"
-    t.string   "subject",              :default => ""
-    t.integer  "sender_id"
-    t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                :default => false
-    t.datetime "updated_at",                              :null => false
-    t.datetime "created_at",                              :null => false
-    t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "notification_code"
-    t.string   "attachment"
-  end
-
-  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
 
   create_table "ownerships", :force => true do |t|
     t.integer  "person_id"
@@ -97,7 +73,6 @@ ActiveRecord::Schema.define(:version => 20130323181331) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "avatarPath"
   end
 
   create_table "people_projects", :id => false, :force => true do |t|
@@ -130,20 +105,6 @@ ActiveRecord::Schema.define(:version => 20130323181331) do
   end
 
   add_index "projects", ["person_id"], :name => "index_projects_on_person_id"
-
-  create_table "receipts", :force => true do |t|
-    t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                                  :null => false
-    t.boolean  "is_read",                       :default => false
-    t.boolean  "trashed",                       :default => false
-    t.boolean  "deleted",                       :default => false
-    t.string   "mailbox_type",    :limit => 25
-    t.datetime "created_at",                                       :null => false
-    t.datetime "updated_at",                                       :null => false
-  end
-
-  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
