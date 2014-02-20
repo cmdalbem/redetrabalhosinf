891f400fd3c907c9b2cb436a1340cddbfe58a9fc
Cristiano M. Dalbem <ninja.dalbem@gmail.com>
2014-02-20 23:18:47 +0100

    Recovers Gemfile Not really sure if all versions on it are correct, but the list of gems is.

--- inf/db/schema.rb
+++ inf/db/schema.rb
@@ -31,11 +31,6 @@ ActiveRecord::Schema.define(:version => 20130718070817) do
   add_index "activities", ["recipient_id", "recipient_type"], :name => "index_activities_on_recipient_id_and_recipient_type"
   add_index "activities", ["trackable_id", "trackable_type"], :name => "index_activities_on_trackable_id_and_trackable_type"
 
-  create_table "activities_users", :force => true do |t|
-    t.integer "activity_id"
-    t.integer "user_id"
-  end
-
   create_table "comments", :force => true do |t|
     t.integer  "person_id"
     t.text     "text"
@@ -46,12 +41,6 @@ ActiveRecord::Schema.define(:version => 20130718070817) do
 
   add_index "comments", ["person_id"], :name => "index_comments_on_person_id"
   add_index "comments", ["project_id"], :name => "index_comments_on_project_id"
-
-  create_table "conversations", :force => true do |t|
-    t.string   "subject",    :default => ""
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-  end
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -67,24 +56,6 @@ ActiveRecord::Schema.define(:version => 20130718070817) do
     t.datetime "updated_at",                :null => false
     t.integer  "hits",       :default => 0
   end
-
-  create_table "notifications", :force => true do |t|
-    t.string   "type"
-    t.text     "body"
-    t.string   "subject",              :default => ""
-    t.integer  "sender_id"
-    t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                :default => false
-    t.datetime "updated_at",                              :null => false
-    t.datetime "created_at",                              :null => false
-    t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "notification_code"
-    t.string   "attachment"
-  end
-
-  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
 
   create_table "ownerships", :force => true do |t|
     t.integer  "person_id"
@@ -111,7 +82,6 @@ ActiveRecord::Schema.define(:version => 20130718070817) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "avatarPath"
   end
 
   create_table "people_projects", :id => false, :force => true do |t|
@@ -150,20 +120,6 @@ ActiveRecord::Schema.define(:version => 20130718070817) do
   end
 
   add_index "projects", ["person_id"], :name => "index_projects_on_person_id"
-
-  create_table "receipts", :force => true do |t|
-    t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                                  :null => false
-    t.boolean  "is_read",                       :default => false
-    t.boolean  "trashed",                       :default => false
-    t.boolean  "deleted",                       :default => false
-    t.string   "mailbox_type",    :limit => 25
-    t.datetime "created_at",                                       :null => false
-    t.datetime "updated_at",                                       :null => false
-  end
-
-  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
 
   create_table "search_logs", :force => true do |t|
     t.text     "text"
